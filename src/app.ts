a6240836df556069c379ac624bf4d3829ab6b48a
AH-Likhon <ictianlikhon6@gmail.com>
2023-05-30 12:02:53 +0600

    [DONE] users route, controller, service

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,17 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
+import usersRouter from './app/modules/users/users.route'
+
 app.use(cors())
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// application routes
+app.use('/api/v1/users', usersRouter)
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('Hello World is working')
 })
 
